63236ef38660d7382e413bf052fde52d3834fbda
adriroca20 <hardhe21@gmail.com>
2022-02-23 00:53:00 +0100

    Demo creacion de preguntas

---
+++ DemoCreacionPreguntas/DemoCreacionPreguntas/TipoTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoCreacionPreguntas
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            // open file dialog   
+            OpenFileDialog open = new OpenFileDialog();
+            // image filters  
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                // display image in picture box  
+                imagenPregunta.Image = new Bitmap(open.FileName);
+            }
+        }
+    }
+}
