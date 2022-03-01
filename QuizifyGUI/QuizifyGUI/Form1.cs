e76289699c438ecf7902fe94f3abda2dd475e391
Microhard1 <microhardquiz@gmail.com>
2022-03-01 15:26:07 +0100

    Fin maqueta crear preguntas

--- QuizifyGUI/QuizifyGUI/Form1.cs
+++ QuizifyGUI/QuizifyGUI/Form1.cs
@@ -47,5 +47,15 @@ namespace QuizifyGUI
         {
 
         }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
