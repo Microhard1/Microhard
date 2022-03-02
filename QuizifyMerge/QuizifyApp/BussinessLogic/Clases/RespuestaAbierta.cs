9e7d67a3ab023b43a1f85e90838ad155338f139c
Microhard1 <microhardquiz@gmail.com>
2022-03-02 15:21:44 +0100

    Interfaz y distribución por carpetas unidas

---
+++ QuizifyMerge/QuizifyApp/BussinessLogic/Clases/RespuestaAbierta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizifyApp.BussinessLogic.Clases
+{
+    class RespuestaAbierta : Respuesta
+    {
+      
+        public RespuestaAbierta(string enunciado, bool correcta) : base(enunciado)
+        {
+
+           
+            
+        }
+    }
+}
